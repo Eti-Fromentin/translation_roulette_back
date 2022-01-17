138d08456a1aa8d484211943ae170199a89dd692
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-17 02:03:13 +0100

    random translation v1 working

--- controller/randomRequestController.js
+++ controller/randomRequestController.js
@@ -3,7 +3,7 @@ const { postRandom } = require('../model/randomRequestModel');
 const reqRandom = async (req, res) => {
     const body = req.body;
     const data = await postRandom(body);
-    console.table(data[0].translations);
+    console.table(data);
     res.status(200).send(data);
 }
 
--- model/randomRequestModel.js
+++ model/randomRequestModel.js
@@ -2,73 +2,89 @@ const { KEY1, ENDPOINT } = require("../env");
 const { v4: uuidv4 } = require("uuid");
 const { default: axios } = require("axios");
 
-const postRandom = async (body) => {
-  const lang = [
-    "af",
-    "sq",
-    "ar",
-    "hy",
-    "bg",
-    "ca",
-    "zh-Hans",
-    "zh-Hant",
-    "hr",
-    "cs",
-    "da",
-    "dv",
-    "nl",
-    "en",
-    "et",
-    "fi",
-    "fr-ca",
-    "ka",
-    "de",
-    "el",
-    "gu",
-    "ht",
-    "he",
-    "hi",
-    "hu",
-    "is",
-    "id",
-    "iu",
-    "ga",
-    "it",
-    "ja",
-    "km",
-    "ko",
-    "ku",
-    "lo",
-    "lv",
-    "lt",
-    "mk",
-  ];
-  const result = await axios({
-    baseURL: ENDPOINT,
-    url: "/translate",
-    method: "post",
-    headers: {
-      "Ocp-Apim-Subscription-Key": KEY1,
-      "Ocp-Apim-Subscription-Region": "westeurope",
-      "Content-type": "application/json",
-      "X-ClientTraceId": uuidv4().toString(),
-    },
-    params: {
-      "api-version": "3.0",
-    //   'from': 'en',
-      'to': body.to,
-    },
-    data: [
-      {
-        text: body.text,
-      },
-    ],
-    responseType: "json",
-  });
-  //   .then(function (response) {
-  //     console.log(JSON.stringify(response.data, null, 4));
-  //   });
-  return result.data;
-};
+const lang = [
+  "af",
+  "sq",
+  "ar",
+  "bg",
+  "ca",
+  "zh-Hans",
+  "zh-Hant",
+  "hr",
+  "cs",
+  "da",
+  "nl",
+  "en",
+  "et",
+  "fi",
+  "de",
+  "el",
+  "gu",
+  "ht",
+  "he",
+  "hi",
+  "hu",
+  "is",
+  "id",
+  "ga",
+  "it",
+  "ja",
+  "km",
+  "ko",
+  "lv",
+  "lt",
+  "mk",
+];
 
+const postRandom = async (body) => {
+  const randomLang = lang
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+    .slice(0, body.number)
+    .concat("fr");
+
+  console.log(randomLang);
+  let result = [];
+
+  const results = await randomLang.reduce(async (memo, elt) => {
+    await memo;
+    const from = result.length === 0 ? "fr" : randomLang[result.length];
+    const text = result.length === 0 ? body.text : result[result.length - 1][0].translations[0].text;
+    console.log(result);
+    console.log(from);
+    console.log(text);
+    return axios({
+      baseURL: ENDPOINT,
+      url: "/translate",
+      method: "post",
+      headers: {
+        "Ocp-Apim-Subscription-Key": KEY1,
+        "Ocp-Apim-Subscription-Region": "westeurope",
+        "Content-type": "application/json",
+        "X-ClientTraceId": uuidv4().toString(),
+      },
+      params: {
+        "api-version": "3.0",
+        toScript: "latn",
+        from: from,
+        to: elt,
+      },
+      data: [
+        {
+          text: text,
+        },
+      ],
+      responseType: "json",
+    })
+      .then((response) => {
+        const [ data ] = response.data;
+        result.push([ data ]);
+      })
+      .catch((err) => console.error(err));
+  });
+
+  return Promise.all(result);
+
+};
 module.exports = { postRandom };
